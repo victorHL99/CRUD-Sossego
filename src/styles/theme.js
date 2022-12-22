f0ea98f98d20bdacf02d11d77110ea470fa94ed5
victorHL99 <victor.lessa@estudante.ufla.br>
2022-12-22 11:16:40 -0300

    feat: :lipstick: added colors for theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,7 +2,12 @@ const theme = {
   fonts: {
     primary: "'Poppins', sans-serif",
   },
-  colors: {},
+  colors: {
+    lightGray: '#F0F2F6',
+    darkGray: '#8C98A9',
+    purple: '#5357B1',
+    green: '#00AE63',
+  },
   breakpoints: {
     mobile: '320px',
     tablet: '481px',
