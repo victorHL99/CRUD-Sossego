b4a5aacfc16ef243b559afe6e3807282c944a919
victorHL99 <victor.lessa@estudante.ufla.br>
2022-12-22 11:12:06 -0300

    feat: :lipstick: added primary theme, with fonts, colors and breackpoints

---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+const theme = {
+  fonts: {
+    primary: "'Poppins', sans-serif",
+  },
+  colors: {},
+  breakpoints: {
+    mobile: '320px',
+    tablet: '481px',
+    laptop: '769px',
+    largeScreen: '1025px',
+    desktop: '1279px',
+  },
+};
+
+export default theme;
