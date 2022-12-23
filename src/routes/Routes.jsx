3b2bacae02983475cf30e4d31c9f4ca806b6f254
victorHL99 <victor.lessa@estudante.ufla.br>
2022-12-23 14:51:42 -0300

    feat: create route for Signup at the new users

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
+import Signup from '../components/pages/Signup';
 
 export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
+      <Route path="/" element={<Signup />} />
     </Routes>
   );
 }
