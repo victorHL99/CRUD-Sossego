5baea3b7dec5cf83b9d4af44cf7790f25754fca8
victorHL99 <victor.lessa@estudante.ufla.br>
2022-12-24 16:43:33 -0300

    refactor: :recycle: added parameters for get values at the inputs

--- src/components/Dashboard/Input/index.jsx
+++ src/components/Dashboard/Input/index.jsx
@@ -1,10 +1,23 @@
 import * as S from './styles';
 
-export default function Input({ label, type, placeholder, width, height }) {
+export default function Input({
+  label,
+  type,
+  placeholder,
+  width,
+  height,
+  onInput,
+  onSetInput,
+}) {
   return (
     <S.InputContainer width={width} height={height} label={label}>
       <S.InputLabel>{label}</S.InputLabel>
-      <S.InputField type={type} placeholder={placeholder} />
+      <S.InputField
+        type={type}
+        placeholder={placeholder}
+        value={onInput}
+        onChange={e => onSetInput(e.target.value)}
+      />
     </S.InputContainer>
   );
 }
