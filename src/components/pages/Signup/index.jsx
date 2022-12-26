acaef5c6373a11a1535f9f1c040733adcdc498a2
victorHL99 <victor.lessa@estudante.ufla.br>
2022-12-26 16:19:59 -0300

    refactor: :recycle: added condition for stage ===4

--- src/components/pages/Signup/index.jsx
+++ src/components/pages/Signup/index.jsx
@@ -1,13 +1,17 @@
+import { useContext } from 'react';
 import Dashboard from '../../Dashboard';
 import BackgroundPage from '../../BackgroundPage';
 import * as S from './styles';
+import StageContext from '../../../contexts/StageContext';
+import FinishDashboard from '../../FinishDashboard';
 
 export default function Signup() {
+  const { stage } = useContext(StageContext);
   return (
     <S.SignupContainer>
       <BackgroundPage position="left" />
       <BackgroundPage position="right" />
-      <Dashboard />
+      {stage !== 4 ? <Dashboard /> : <FinishDashboard />}
     </S.SignupContainer>
   );
 }
