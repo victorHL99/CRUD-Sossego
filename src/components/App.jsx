826d4a9ac2041ec422d26fd5eb3e2f1eee5a8c63
victorHL99 <victor.lessa@estudante.ufla.br>
2022-12-26 16:56:08 -0300

    refactor: :recycle: remake user context

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,21 +4,61 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import StageContext from '../contexts/StageContext';
+import UserContext from '../contexts/UserContext';
 import GlobalStyle from '../styles/globalStyle';
 import theme from '../styles/theme';
 import Routes from '../routes/Routes';
 
 function App() {
   const [stage, setStage] = React.useState(1);
+  const [name, setName] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [date, setDate] = React.useState('');
+  const [cep, setCep] = React.useState('');
+  const [street, setStreet] = React.useState('');
+  const [number, setNumber] = React.useState('');
+  const [district, setDistrict] = React.useState('');
+  const [city, setCity] = React.useState('');
+  const [reference, setReference] = React.useState('');
+  const [about, setAbout] = React.useState('');
+
   return (
-    <StageContext.Provider value={{ stage, setStage }}>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <GlobalStyle />
-          <Routes />
-        </BrowserRouter>
-      </ThemeProvider>
-    </StageContext.Provider>
+    <UserContext.Provider
+      value={{
+        name,
+        setName,
+        password,
+        setPassword,
+        email,
+        setEmail,
+        date,
+        setDate,
+        cep,
+        setCep,
+        street,
+        setStreet,
+        number,
+        setNumber,
+        district,
+        setDistrict,
+        city,
+        setCity,
+        reference,
+        setReference,
+        about,
+        setAbout,
+      }}
+    >
+      <StageContext.Provider value={{ stage, setStage }}>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <GlobalStyle />
+            <Routes />
+          </BrowserRouter>
+        </ThemeProvider>
+      </StageContext.Provider>
+    </UserContext.Provider>
   );
 }
 
