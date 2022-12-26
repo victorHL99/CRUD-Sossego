7ff7ed09166f6c0bfdd6a67d1dfa18b30c13946f
victorHL99 <victor.lessa@estudante.ufla.br>
2022-12-26 14:34:55 -0300

    feat: :sparkles: added context to stage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,24 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
+import StageContext from '../contexts/StageContext';
 import GlobalStyle from '../styles/globalStyle';
 import theme from '../styles/theme';
 import Routes from '../routes/Routes';
 
 function App() {
+  const [stage, setStage] = React.useState(1);
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <GlobalStyle />
-        <Routes />
-      </BrowserRouter>
-    </ThemeProvider>
+    <StageContext.Provider value={{ stage, setStage }}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <GlobalStyle />
+          <Routes />
+        </BrowserRouter>
+      </ThemeProvider>
+    </StageContext.Provider>
   );
 }
 
