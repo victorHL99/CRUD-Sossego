87a5c7175792a34c4af858e2951a7649074e05d5
victorHL99 <victor.lessa@estudante.ufla.br>
2022-12-27 15:50:24 -0300

    feat: :sparkles: create component for final page

--- src/components/FinishDashboard/index.jsx
+++ src/components/FinishDashboard/index.jsx
@@ -1,9 +1,42 @@
+import { useContext } from 'react';
+import UserContext from '../../contexts/UserContext';
 import * as S from './styles';
+import Buttom from '../Dashboard/Button';
+import BoxInfo from './BoxInfo';
 
 export default function FinishDashboard() {
+  const { name, email, street, number, cep } = useContext(UserContext);
+
+  function formatCep() {
+    return cep.replace(/(\d{5})(\d{3})/, '$1-$2');
+  }
+
   return (
     <S.FinishContainer>
       <S.Title>Usuário Criado!</S.Title>
+      <S.FinalForm>
+        <S.TopSubContainer>
+          <BoxInfo title="Nome" text={name} />
+          <BoxInfo title="Email" text={email} />
+        </S.TopSubContainer>
+        <S.DividingLine />
+        <S.BottomContainer>
+          <BoxInfo title="Endereço" text={street} />
+          <BoxInfo title="Número" text={number} />
+          <BoxInfo title="CEP" text={formatCep()} />
+        </S.BottomContainer>
+      </S.FinalForm>
+      <S.ButtonContainer>
+        <Buttom
+          onClick={() => {
+            console.log('testeee');
+          }}
+          color="#5357B1"
+          text="Novo usuário"
+          width="442"
+          margin="40"
+        />
+      </S.ButtonContainer>
     </S.FinishContainer>
   );
 }
--- src/components/FinishDashboard/styles.jsx
+++ src/components/FinishDashboard/styles.jsx
@@ -22,3 +22,40 @@ export const Title = styled.h1`
   line-height: 54px;
   color: ${({ theme }) => theme.colors.black};
 `;
+
+export const FinalForm = styled.div`
+  margin: 34px 0 0 69px;
+  width: 442px;
+  height: 100%;
+`;
+
+export const TopSubContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  margin-bottom: 20px;
+`;
+
+export const BottomContainer = styled.div`
+  margin-top: 27px;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 24px;
+`;
+
+export const DividingLine = styled.div`
+  width: 100%;
+  height: 1px;
+  background: ${({ theme }) => theme.colors.lightGray};
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: auto;
+  margin-top: 40px;
+  margin-bottom: 40px;
+  margin-left: 55px;
+`;
